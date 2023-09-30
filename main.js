5882a3c785365174eff13b165fcab3553eb2f336
kleentinz <jubrilrasheed2020@gmail.com>
2023-09-30 12:17:41 -0500

    #firstCommit

---
+++ main.js
@@ -0,0 +1,14 @@
+// Array of words to rotate
+const words = ["Yourself", "Men", "Women", "Kids"];
+let currentWordIndex = 0; // Index of the current word
+
+// Function to update the heading text with the next word
+function rotateWord() {
+  document.getElementById("rotatingHeading").textContent =
+    "Get the best for " + words[currentWordIndex];
+  currentWordIndex = (currentWordIndex + 1) % words.length; // Cycle through the words
+}
+
+// Call rotateWord initially and set an interval to call it repeatedly
+rotateWord();
+setInterval(rotateWord, 2000); // Change word every 2 seconds (adjust as needed)
